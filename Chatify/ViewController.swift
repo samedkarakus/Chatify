817b3c6f7e8a4513cb224a75ddf0a857e9d7bf11
samedkarakus <samedkarakus.ceng@gmail.com>
2024-08-24 17:17:24 +0300

    Initial Commit

---
+++ Chatify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chatify
+//
+//  Created by Samed Karakuş on 24.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
